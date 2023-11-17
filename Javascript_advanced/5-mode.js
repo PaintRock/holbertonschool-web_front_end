f63608692db723e90131a800a1a00381b29d4884
PaintRock <6126@holbertonstudents.com>
2023-11-17 20:02:37 +0000

    all

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -1,41 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Theme Changer</title>
-</head>
-<body>
-  <p id="welcomeText">Welcome Holberton!</p>
-  <button onclick="spooky()">Spooky</button>
-  <button onclick="darkMode()">Dark mode</button>
-  <button onclick="screamMode()">Scream mode</button>
+// 5-mode.js
 
-  <script>
-    function changeMode(size, weight, transform, background, color) {
-      return function() {
-        document.body.style.fontSize = `${size}px`;
-        document.body.style.fontWeight = weight;
-        document.body.style.textTransform = transform;
-        document.body.style.backgroundColor = background;
-        document.body.style.color = color;
-      };
-    }
+function changeMode(size, weight, transform, background, color) {
+  return function () {
+    document.body.style.fontSize = size + 'px';
+    document.body.style.fontWeight = weight;
+    document.body.style.textTransform = transform;
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
+  };
+}
 
-    function main() {
-      const spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
-      const darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
-      const screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
+function main() {
+  const spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+  const darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
+  const screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
 
-      document.getElementById('welcomeText').style.marginBottom = '20px';
+  // Create paragraph
+  const welcomeParagraph = document.createElement('p');
+  welcomeParagraph.textContent = 'Welcome Holberton!';
+  document.body.appendChild(welcomeParagraph);
 
-      document.querySelector('button:nth-child(1)').addEventListener('click', spooky);
-      document.querySelector('button:nth-child(2)').addEventListener('click', darkMode);
-      document.querySelector('button:nth-child(3)').addEventListener('click', screamMode);
-    }
+  // Create Spooky button
+  const spookyButton = document.createElement('button');
+  spookyButton.textContent = 'Spooky';
+  spookyButton.addEventListener('click', spooky);
+  document.body.appendChild(spookyButton);
 
-    main();
-  </script>
-</body>
-</html>
- 
+  // Create Dark mode button
+  const darkModeButton = document.createElement('button');
+  darkModeButton.textContent = 'Dark mode';
+  darkModeButton.addEventListener('click', darkMode);
+  document.body.appendChild(darkModeButton);
+
+  // Create Scream mode button
+  const screamModeButton = document.createElement('button');
+  screamModeButton.textContent = 'Scream mode';
+  screamModeButton.addEventListener('click', screamMode);
+  document.body.appendChild(screamModeButton);
+}
+
+// Call the main function when the page is loaded
+document.addEventListener('DOMContentLoaded', main);
